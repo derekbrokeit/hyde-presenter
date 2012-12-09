95dcc5a2d5b8fc63cd1803b844299b7a11b0ca7a
Derek Ashley Thomas <derekathomas@gmail.com>
2012-12-09 13:45:41 +0900

    Refactored the presenter plugin to make it more intuitive to configure

--- hyde_ext/presenter.py
+++ hyde_ext/presenter.py
@@ -13,29 +13,48 @@ import sys
 
 '''
 
-PresenterPlugin allows the generation of impress.js presentations
-on the fly from text files. This allows you to host stunning, dynamic
-presentation on the web.
+PresenterPlugin allows the generation of complex html structures in
+easy to read files. This allows you to quickly host stunning, dynamic
+presentation on the web. With the ability to write very simple layouts
+and automatically generate the html markup makes this plugin incredibly
+powerful.
 
-Slides are encased in dumbbell blocks '==--=='. Each slide can have
-custom meta data added to the step element in a slide specific
-yaml-front-matter. Customized default filters can be applied to the
-slides from the sites.yaml configuration settings. No customization
-means no filter is applied.
+The structures in presenter are two fold: 1) Presentation and 2) slides.
+The idea si that presentations are the enclosing html element with
+slides sequentially layed out afterward. This can make anything form
+slideshows, headers, and even impress.js presentations with ease.
 
-Example two slide layout:
+As an example, the following is a two-slide layout:
 
-==--==
+∂∂ my-presentation
+………………
 This is a slide
 this is still on the first slide
 
-==--==
----
+………………
 # yaml matter goes here
 id: test
+class: foo
 ---
 slide **two** text
-==--==
+………………
+∂∂ /my-presentation
+
+As you can see, a presentation is enclosed in '∂∂' blocks with an
+id-tag 'my-presentation' (whatever you want). Each slide can have it's
+meta data specified in a yaml-header. Meta data takes the form of tag
+properties 'class="foo" id="test"'. This can be set in the resource meta
+data or in the sites.yaml configurations under 'presenter'.
+
+You can also add slide enumeration to class and id attributes by setting
+the meta value to include '%d' for the slide-id-number. After the
+presentation has been processed, it can be retrieved and used in the
+`resource.pres` list. A resource can have as many presentations as
+desired.
+
+Note: For impress.js presentations, every slide should have a "step"
+class and the presentation id should be "impress".
+
 '''
 
 PROP        = u"%s='%s'"
@@ -46,13 +65,10 @@ ELEMENT     = "%s\n%s\n%s"
 
 YAML_FINDER = "---"
 
-#RE_SLIDE = re.compile(r'(?<=^==--==\n)(.*\n)+?(?=^==--==$)', re.MULTILINE)
-#RE_YAML  = re.compile(r'(?<=^---\n)(.*\n)+?(?=^---$)', re.MULTILINE)
-
 FENCE_CHAR = u'…'
 FENCE_REP  = 5
 SLIDE_RE = re.compile( \
-        ur'(?P<fence>^(?:%s{%d,}))(?:…*)[ ]*(\{?\.?(?P<comment>[a-zA-Z0-9_-]*)\}?)?[ ]*\n(?P<slide>.*?)(?<=\n)(?P=fence)' % (FENCE_CHAR, FENCE_REP),
+        ur'(?P<fence>^(?:%s{%d,}))(?:%s*)[ ]*(\{?\.?(?P<comment>[a-zA-Z0-9_-]*)\}?)?[ ]*\n(?P<slide>.*?)(?<=\n)(?P=fence)' % (FENCE_CHAR, FENCE_REP, FENCE_CHAR),
     #ur'(?P<fence>^(?:…{5,}))[ ]*(\{?\.?(?P<comment>[a-zA-Z0-9_-]*)\}?)?[ ]*\n(?P<slide>.*?)(?<=\n)(?P=fence)[ ]*$',
     re.UNICODE|re.MULTILINE|re.DOTALL
     )
@@ -60,31 +76,34 @@ SLIDE_RE = re.compile( \
 
 
 config_cache = {}
-defaults = {    "element":         "div",
-                "outer_element" :  "div",
-                "filter" :         None,
-                "id_N" :           None,
-                "class_N":         None,
+default = {
+                "element":  "div", # element of the outer presentation
+                "meta":    None,   # class of the outer presentation
+                }
+slide_default = {
+                "filter" :  None,  # filter used on each individual slide
+                "element":  "div", # element of individual slides
+                "meta": None,
             }
+
 
 class HTMLStructure(Expando):
     '''
     A structure that represents an html element with a start-tag and end-tag with
-    text between the tags.
+    text between the tags. The properties of this structure are held in the meta-attribute.
     '''
-    def __init__(self, env, raw_text, filter, element, id_N, class_N, meta=None):
-        self.env = env
+    def __init__(self, raw_text, element, meta=None):
         self.raw_text = raw_text
-        self.filter = filter
-        self.element = element
-        self.id_N = id_N
-        self.class_N = class_N
-        self.meta = meta
+        self.element  = element
+        if meta:
+            self.meta     = Expando(meta)
+        else:
+            self.meta = Expando({})
 
     @property
     def props(self):
         props = ""
-        for key_val in self.meta.items():
+        for key_val in self.meta.__dict__.items():
             props += " " + PROP % key_val
 
         return props
@@ -107,22 +126,24 @@ class HTMLStructure(Expando):
 
 
 class Presentation(HTMLStructure):
-    def __init__(self, outer_element=None, **kwargs):
-        super(Presentation, self).__init__(**kwargs)
+    '''
+    A Presentation structure holds a series of slides for presentation
+    '''
+    def __init__(self, raw_text, element, meta, slide_items):
 
-        self.inner_element = self.element
-        self.element = outer_element
+        super(Presentation, self).__init__(raw_text, element, meta)
+
+        self.slide_items = slide_items
         self.slides = []
         self._generate_slides()
 
     def _generate_slides(self):
+        '''
+        build the slides list
+        '''
 
         i = 0
         text = self.raw_text
-        is_impress = (self.meta["id"] == "impress") # take precautions to check if this is for impress.js
-        general_input = {"is_impress":is_impress, "element":self.inner_element,
-                "id_N":self.id_N, "class_N":self.class_N, "filter":self.filter,
-                "env": self.env}
         while True:
             s =  SLIDE_RE.search(text)
             if s:
@@ -131,9 +152,9 @@ class Presentation(HTMLStructure):
                     comment = COMMENT_TAG % comment
                 slide_text = s.group('slide')
 
-                self.slides.append(Slide(idx=i, comment=comment,
-                    raw_text=slide_text, **general_input))
-
+                self.slides.append(
+                        Slide(slide_text, idx=i, comment=comment, **self.slide_items)
+                        )
             else:
                 break
 
@@ -154,74 +175,62 @@ class Presentation(HTMLStructure):
 
 class Slide(HTMLStructure):
     '''
-    A convenient class that represents a slide in impres.js.
-    It takes in the whole text of the slide and produces
-    an html div element for impress to work with.
+    A repsresentation of a slide to present a small snippet of text or html
     '''
-    def __init__(self, idx, comment=None, is_impress=False, **kwargs):
-        super(Slide, self).__init__(**kwargs)
+    def __init__(self, raw_text, element, meta, idx, env, filter=None, comment=None):
+        super(Slide, self).__init__(raw_text, element, meta)
 
-        self.idx        = idx
-        self.comment    = comment
-        self.is_impress = is_impress
+        self.env     = env
+        self.filter  = filter
+        self.idx     = idx
+        self.comment = comment
 
         self._parse_raw_text()
 
-
     def _parse_raw_text(self):
+        '''
+        find the meta data and text
+        '''
         # first, find yaml matter,
         # assuming only the bottom '---' to save space
         idx_ym = self.raw_text.find(YAML_FINDER, len(YAML_FINDER))
         if idx_ym > 0:
             ym = self.raw_text[:idx_ym-1]
-        elif self.is_impress:
-            ym = "class: step"
         else:
             ym = ''
 
         # make the meta dict
-        self.meta = yaml.load(ym)
-        if not self.meta:
+        meta = Expando(yaml.load(ym))
+        if not meta:
             # if meta is None, make is a dict
-            self.meta = {}
+            meta = Expando({})
         elif isinstance(self.meta, str):
             # if meta ends up as a string, assume it's empty
-            self.meta = {}
+            meta = Expando({})
             idx_ym = -1
 
-        # then add the step class if necessary which
-        # signals a single slide in impress.js
-        if self.is_impress:
-            try:
-                if "step" not in self.meta["class"]:
-                    self.meta["class"] += " step"
-            except KeyError:
-                self.meta["class"] = "step"
-
+        # if a meta collision occurs, the yaml-matter is prefered
+        # except in the case of 'class', which appends it to the existing version
         try:
-            # override the element
-            self.element = self.meta["element"]
-            del self.meta["element"]
-        except KeyError:
+            old_class = getattr(self.meta, "class")
+            new_class = old_class + " " + getattr(meta, "class")
+            setattr(self.meta, "class", new_class)
+            delattr(meta, "class")
+        except AttributeError:
+            pass
+        self.meta.update(meta)
+
+        # attempt to enumerate class and id, if possible
+        try:
+            setattr(self.meta, "id", self.meta.id % self.idx)
+        except (AttributeError, TypeError):
+            pass
+        try:
+            setattr(self.meta, "class", getattr(self.meta, "class") % self.idx)
+        except (AttributeError, TypeError):
             pass
 
-        try:
-            # override filter
-            self.filter = self.meta["filter"]
-            del self.meta["filter"]
-        except KeyError:
-            pass
-
-        if self.id_N:
-            self.meta["id_N"] = self.id_N % self.idx
-
-        if self.class_N:
-            try:
-                self.meta["class"] += " " + self.class_N % self.idx
-            except KeyError:
-                self.meta["class"] = self.class_N % self.idx
-
-        # now find the standard text and filter it
+        # now find the standard text
         if idx_ym > 0:
             self.slide_text = self.raw_text[idx_ym+len(YAML_FINDER):]
         else:
@@ -247,32 +256,69 @@ class Slide(HTMLStructure):
         return text
 
 @environmentfilter
-def presenter(env, raw_text, id=None, classes=None, resource=None):
+def presenter(env, raw_text, id=None, resource=None):
+    '''
+    A filter that builds a presentation
+    '''
+    # get the config data relevant to this presentation's settings
+    try:
+        res_meta = getattr(resource.meta, "presenter")
+    except AttributeError:
+        res_meta = None
+    try:
+        res_meta_id = getattr(res_meta, id)
+    except AttributeError:
+        res_meta_id = None
+    try:
+        conf_id = getattr(config_cache, id)
+    except AttributeError:
+        conf_id = None
 
-    # create the presentations meta data
-    meta = {}
-    if  id:
-        meta["id"] = id
-    if classes:
-        meta["class"] = classes
-
-    settings = {"meta": meta}
-    # build up the list of keyword arguments to build a presentation
-    # Setting preference starts at the resource, then sites.yaml, then
-    # the 'defaults' dict. Actually, element and filter can even be set
-    # per element in the element-yaml-matter
-    for k in defaults.keys():
+    # gather relevant settings for the presentation and slides
+    settings = {
+            "raw_text": raw_text,
+            "slide_items": {
+                "env": env
+                }
+            }
+    # look for the proper settings for the presentation
+    for k,v in default.items():
         try:
-            settings[k] = getattr(resource.meta, k)
+            # try resource.meta.presenter.KEY
+            settings[k] = getattr(res_meta, k)
         except AttributeError:
             try:
-                settings[k] = getattr(config_cache, k)
+                # try resource.meta.presenter.ID.KEY
+                settings[k] = getattr(res_meta_id, k)
             except AttributeError:
-                settings[k] = defaults[k]
-
-    # add environment and raw text to the settings
-    settings["env"] = env
-    settings["raw_text"] = raw_text
+                try:
+                    # try config.presenter.ID.KEY
+                    settings[k] = getattr(conf_id, k)
+                except AttributeError:
+                    # take the default value
+                    settings[k] = v
+    # set the id meta data
+    if id:
+        if settings["meta"]:
+            settings["meta"].update({"id": id})
+        else:
+            settings["meta"] = Expando({"id": id})
+    # look for the proper settings for the slides
+    for k,v in slide_default.items():
+        try:
+            # try resource.meta.presenter.slides.KEY
+            settings["slide_items"][k] = getattr(getattr(res_meta,"slides"), k)
+        except AttributeError:
+            try:
+                # try resource.meta.presenter.slides.ID.KEY
+                settings["slide_items"][k] = getattr(getattr(res_meta_id,"slides"), k)
+            except AttributeError:
+                try:
+                    # try config.presenter.slides.ID.KEY
+                    settings["slide_items"][k] = getattr(getattr(conf_id,"slides"), k)
+                except AttributeError:
+                    # take the default value
+                    settings["slide_items"][k] = v
 
     # get the resource's presentation list
     try:
@@ -302,13 +348,6 @@ class Presenter(Extension):
         # now we parse a single expression that is used as cache key.
         args = [parser.parse_expression()]
 
-        # if there is a comma, the user provided classes.  If not use
-        # None as second parameter.
-        if parser.stream.skip_if('comma'):
-            args.append(parser.parse_expression())
-        else:
-            args.append(nodes.Const(None))
-
         # end tag
         body = parser.parse_statements(['name:endpresenter'], drop_needle=True)
 
@@ -323,18 +362,20 @@ class Presenter(Extension):
                     self.call_method('_render_presenter',args),
                         [], [], body).set_lineno(lineno)
 
-    def _render_presenter(self, id=None, classes=None, resource=None, caller=None):
+    def _render_presenter(self, id=None, resource=None, caller=None):
         """
         Calls the presenter filter to transform the output.
         """
         if not caller:
             return ''
         output = caller().strip()
-        return presenter(self.environment, output, id, classes, resource)
+        return presenter(self.environment, output, id, resource)
 
 
 class PresenterPlugin(TextyPlugin):
     '''
+    A hyde plugin to aid in the creation of easy to present html-structures and
+    helper for the Presenter Jinja2 extension
     '''
     def __init__(self, site):
         super(PresenterPlugin, self).__init__(site)
@@ -344,9 +385,26 @@ class PresenterPlugin(TextyPlugin):
         caches the site configs for this plugin so that the jinja extension
         can access them
         '''
-        global config_cache
+        global config_cache, default, slide_default
         config = self.site.config
         config_cache = config.presenter
+        try:
+            d = config_cache.default.__dict__
+            try:
+                s = d["slides"].__dict__
+                del d["slides"]
+            except KeyError:
+                s = None
+        except AttributeError:
+            d = None
+        if d:
+            # change the default settings
+            for k, v in d.items():
+                default[k] = v
+            if s:
+                # change the default settings for slides
+                for k, v in s.items():
+                    slide_default[k] = v
 
     def template_loaded(self,template):
         '''
